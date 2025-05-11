ecd00364423e13d78c259fe784db479b68c010ec
Gabriel Matos <gsiva505@hotmail.com>
2025-05-11 14:24:25 -0300

    feat: adding features

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
 require('dotenv').config();
 
-client.on('ready', () => {
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
-  if (message.content === '!ping') {
-    message.reply('Pong!');
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error executing that command.', ephemeral: true });
   }
 });
 
